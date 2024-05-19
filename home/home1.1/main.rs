fcf29b8c0f17d385e77b9ca3a50aa4c47f1fcdbd
Anastasiiasyvak <asyvak@kse.org.ua>
2024-05-19 17:24:42 +0300

    Made improvements based on comments

--- home/home1.1/main.rs
+++ home/home1.1/main.rs
@@ -4,11 +4,11 @@ fn main() {
     loop {
         println!("Please enter the list of numbers separated by spaces:");
 
-        let mut list = String::new();
-        io::stdin().read_line(&mut list)
+        let mut input_list = String::new();
+        io::stdin().read_line(&mut input_list)
             .expect("Failed to read line");
 
-        if list.trim().is_empty() {
+        if input_list.trim().is_empty() {
             println!("Input is empty");
             continue;
         }
@@ -17,8 +17,8 @@ fn main() {
         let mut has_negative = false;
         let mut largest_negative: Option<f64> = None;
         let mut valid_input = true;
-        for i in list.split_whitespace() {
-            match i.trim().parse::<f64>() {
+        for num_str in input_list.split_whitespace() {
+            match num_str.trim().parse::<f64>() {
                 Ok(number) => {
                     if number < 0.0 {
                         has_negative = true;
@@ -45,12 +45,12 @@ fn main() {
         }
 
         println!("Do you want to continue? (y/n)");
-        let mut choice = String::new();
-        io::stdin().read_line(&mut choice)
+        let mut restart_choice = String::new();
+        io::stdin().read_line(&mut restart_choice)
             .expect("Failed to read line");
         
-        let choice = choice.trim().to_lowercase();
-        if choice != "y" {
+        let restart_choice = restart_choice.trim().to_lowercase();
+        if restart_choice != "y" {
             println!("Bye!");
             break;
         }
